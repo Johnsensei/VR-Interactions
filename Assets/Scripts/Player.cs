0f2cfe7cec1193ab056e1e2b40b31efe6b8ad879
Johnsensei <johngale@Minas-Tirith.local>
2019-07-13 19:21:05 -0500

    0.0.0
    Initial commit.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float playerSpeed;
+	public Vector3 castlePosition;
+	public GameObject playerObject;
+	public bool enteredCastle;
+
+	public Vector3 startPosition;
+	private Vector3 targetPosition;
+
+	private float distanceToMove = 22f;
+
+	// Use this for initialization
+	void Start () {
+
+		targetPosition = startPosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+			if (hit.transform.GetComponent<DoorButton> () != null) {
+				hit.transform.GetComponent<DoorButton> ().OnLook ();
+				MoveToCastle ();
+			}
+
+			playerObject.transform.position = Vector3.Lerp (playerObject.transform.position, startPosition, playerSpeed * Time.deltaTime);
+		}
+
+		playerObject.transform.position = Vector3.Lerp (playerObject.transform.position, targetPosition, Time.deltaTime * playerSpeed);
+	
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")){
+			RaycastHit enemyHit;
+
+			if (Physics.Raycast (transform.position, transform.forward, out enemyHit)) {
+				if (hit.transform.GetComponent<Enemy> () != null) {
+					Destroy (hit.transform.gameObject);
+				}
+			}
+		}
+	
+	}
+
+	private void MoveToCastle(){
+		targetPosition = castlePosition;
+		enteredCastle = true;
+		Debug.Log ("I should be moving to the castle.");
+
+	}
+}
---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour {
+
+	public float playerSpeed;
+	public Vector3 castlePosition;
+
+	public Vector3 playerPosition;
+
+	public static PlayerObject Instance;
+
+	void Awake(){
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		playerPosition = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.Lerp (transform.position, playerPosition, Time.deltaTime * playerSpeed);
+
+	}
+
+	public void MoveToCastle(){
+		playerPosition = castlePosition;
+	}
+}
